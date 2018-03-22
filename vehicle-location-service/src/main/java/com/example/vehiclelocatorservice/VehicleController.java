8c080eccf03791a083a9316f33f2cd0ec91f3d28
sharadg <91043+sharadg@users.noreply.github.com>
2018-03-22 13:51:34 -0500

    adding noargsconstructor

--- vehicle-location-service/src/main/java/com/example/vehiclelocatorservice/VehicleController.java
+++ vehicle-location-service/src/main/java/com/example/vehiclelocatorservice/VehicleController.java
@@ -3,6 +3,8 @@ package com.example.vehiclelocatorservice;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,16 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VehicleController {
 
+/*
+    @Value("${server.port}")
+    int portInstance;
+*/
+
     @RequestMapping(path = "/locate", method = RequestMethod.GET)
     public Vehicle locateVehicle(@RequestParam(name = "vehicle", required = true) String vehicle) {
 
         Vehicle lookupV = new Vehicle(vehicle, "Semi-Truck");
+
+//        System.out.println("Request handled by: " + portInstance);
         return lookupV;
     }
 }
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 class Vehicle {
 
     String id;
